46827739c5d420ffb67add40633c67f56a155757
HyenaLoL (u/Dry_Call_6946) <hyenaloldev@gmail.com>
2026-05-06 14:07:00 +0300

    Add files via upload

---
+++ SheFurP/AssetsSP/JSSP/sheworldmain-script.js
@@ -0,0 +1,25 @@
+
+console.log("SheWorldMain page loaded");
+
+/*scroll buttons*/
+const scrollDown = document.getElementById("scrollDown");
+const scrollUp = document.getElementById("scrollUp");
+
+window.addEventListener("scroll", () => {
+  scrollUp.style.display = window.scrollY > 300 ? "block" : "none";
+  scrollDown.style.display = window.scrollY < (document.body.scrollHeight - window.innerHeight - 300) ? "block" : "none";
+});
+
+scrollDown.addEventListener("click", () => {
+  window.scrollBy({
+    top: window.innerHeight,
+    behavior: "smooth"
+  });
+});
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
